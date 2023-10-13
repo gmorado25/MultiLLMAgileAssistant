b246b083927b8ac247eec05bb7ce7fc5cb4a9ac1
vishwaktadepalli <vishwakn13@gmail.com>
2023-10-13 14:12:05 -0500

    t

--- server/multillm/app/views.py
+++ server/multillm/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def process_request(request):
+    # Process the request here
+    data = {'response': 'Received the request successfully!'}
+    return JsonResponse(data)
+
--- server/multillm/multillm/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import JsonResponse
-
-def process_request(request):
-    # Process the request here
-    data = {'response': 'Received the request successfully!'}
-    return JsonResponse(data)
