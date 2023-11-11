2db6ae115901c8740e14271ea6cc67eee337d1f6
Dmitirii <dmitrii.obideiko@gmail.com>
2023-11-11 14:06:55 -0600

    bug-fix + added a command file for unit testing

--- client/src/app/dashboard/pagemultiLLM.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import multiLLM from './page'
-
-describe('<multiLLM />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<multiLLM />)
-  })
-})
